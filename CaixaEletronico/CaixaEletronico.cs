dce6758386aa38f51f999fdad073fd1cb2e8642a
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-05-27 13:15:46 -0300

    Caixa eletrônico - Iniciando projeto Dojo

---
+++ CaixaEletronico/CaixaEletronico.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronico
+{
+    class CaixaEletronico
+    {
+        private readonly Notas notas;
+
+        public CaixaEletronico(Notas notas)
+        {
+            this.notas = notas;
+        }
+
+        public bool VerificaSaque(double valor)
+        {
+
+            if (valor % 10 == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Saque(double valor)
+        {
+            
+        }
+    }
+}
---
+++ CaixaEletronico/Notas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronico
+{
+    class Notas
+    {
+        public int Nota10 { get; private set; }
+        public int Nota20 { get; private set; }
+        public int Nota50 { get; private set; }
+        public int Nota100 { get; private set; }
+
+        public Notas()
+        {
+
+        }
+
+        
+    }
+}
